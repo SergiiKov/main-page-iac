efcaf72d716480338b7740a68ff9d1cce0a798f7
SergiiKov <sergiikovtun.vn@gmail.com>
2020-06-16 16:26:25 +0300

    add styles for @megia

--- src/components/district-card/district-card.component.jsx
+++ src/components/district-card/district-card.component.jsx
@@ -5,11 +5,13 @@ import './district-card.styles.scss'
 const DistrictsCard = (props) =>{
     const { name, webSite, id, href } = props;
     return(
-        <div className='card-container'>
-            <img src={require(`../../img/${id}.jpg`)} alt='district card' />
-            <h2>{'Район: ' + name}</h2>
-            <a href={href}>{'Веб сайт: ' + webSite}</a>
-        </div>
+       <a href={href} target="_blank" rel="noopener noreferrer">
+            <div className='card-container'>
+                <img src={require(`../../img/${id}.png`)} alt='district card' className='img-district' />
+                <h2>{name}</h2>
+                <a href={href}>{'Cайт: ' + webSite}</a>
+            </div>
+       </a> 
     )
 };
 
--- src/components/districts.js
+++ src/components/districts.js
@@ -1,14 +1,8 @@
 export const districts = [
   {
     id: 1,
-    name: 'Барський',
-    webSite: 'www.test1',
-    href:"http://smp.vin.ua/joomla/"
-  },
-  {
-    id: 2,
-    name: 'Бершадський',
-    webSite: 'www.test2',
-    href:"https://mdbootstrap.com/docs/jquery/css/colors/"
+    name: 'Яришівський психоневрологічний будинок-інтернат',
+    webSite: 'smp.vin.ua/wordpress/yarishiv',
+    href:"http://smp.vin.ua/wordpress/yarishiv/"
   }
 ];
